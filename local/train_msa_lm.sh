1c627fe656c8bed3db23f1739faf40b9578d2f4b
maggieezzat <maggieezzat9@gmail.com>
2020-12-02 21:33:50 +0200

    generate lm data

--- local/train_msa_lm.sh
+++ local/train_msa_lm.sh
@@ -11,7 +11,10 @@ srilm_dir="../../tools/srilm/bin/i686-m64"
 
 ################################################# Data Preparation ##################################################
 #append train transcripts to lm train data and generate words list
-python local/scripts/gen_lm_data.py
+#python local/scripts/gen_lm_data.py
+
+#sort unique to avoid any duplicates
+sort -u -o data/local/lm/lm_train__words_list.txt data/local/lm/lm_train__words_list.txt
 #####################################################################################################################
 
 
